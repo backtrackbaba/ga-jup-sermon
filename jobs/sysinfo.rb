a71cfe8fa570ee6de4e4e46f1dfe0f03f131df3c
Saiprasad Balasubramanian <sai.cosmo@gmail.com>
2018-10-12 17:10:04 +0530

    First Deployment | LSOF

---
+++ jobs/sysinfo.rb
@@ -0,0 +1,11 @@
+
+
+# SCHEDULER.every '5s' do
+#   lsof = `ls | wc -l`
+  
+
+# #   send_event('valuation', { current: current_valuation, last: last_valuation })
+# #   send_event('karma', { current: current_karma, last: last_karma })
+#   send_event('lsof',   { current: (rand(800...200000)) })
+#   send_event('ram', { value: (rand(1...16000)*100) })
+# end
---
+++ scripts/sysinfo.sh
@@ -0,0 +1,7 @@
+# # lsof_value=$(lsof | wc -l)
+# lsof_value=12
+
+# echo $lsof_value
+
+
+# curl -d '{ "auth_token": "YOUR_AUTH_TOKEN", "value": ${lsof_value} }' \http://localhost:3030/widgets/lsof
